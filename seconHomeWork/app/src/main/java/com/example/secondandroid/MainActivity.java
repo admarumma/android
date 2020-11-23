ffb7722eaaa7a6d46f2ab0417777d6af802f8224
admarumma <umar.adilov2002@gmail.com>
2020-11-23 09:22:25 +0500

    My Games in this branch

--- seconHomeWork/app/src/main/java/com/example/secondandroid/MainActivity.java
+++ seconHomeWork/app/src/main/java/com/example/secondandroid/MainActivity.java
@@ -7,43 +7,54 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity  {
 
-    int counter = 0;
+    int random;
+    int tried = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+//        Button btn = (Button) findViewById(R.id.btn);
+//        btn.setOnClickListener(new Button.OnClickListener(){
+//            @Override
+//            public void onClick(View v) {
+//                counter++;
+//                TextView txt = (TextView) findViewById(R.id.txt);
+//                txt.setText(v.getContentDescription());
+//            }
+//        });
+        TextView txt = (TextView) findViewById(R.id.txt);
+        Random random = new Random();
+        this.random = random.nextInt(9);
+        txt.setText("Вы можете начать игру! Выберите число");
+    }
 
-        Button add = (Button) findViewById(R.id.add);
-        Button reduce = (Button) findViewById(R.id.reduce);
-        Button zero = (Button) findViewById(R.id.zero);
-
-        add.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                counter++;
-                TextView txt = (TextView) findViewById(R.id.txt);
-                txt.setText(Integer.toString(counter));
+    public void onBtn(View v){
+        TextView txt = (TextView) findViewById(R.id.txt);
+        if(this.tried <= 5){
+            if(this.random == Integer.parseInt((String) v.getContentDescription())){
+                this.tried = 0;
+                Random random = new Random();
+                this.random = random.nextInt(9);
+                txt.setText("Вы вииграли. Вы можете начать игру! Выберите число");
+            }else{
+                tried++;
+                if(this.random < Integer.parseInt((String) v.getContentDescription())){
+                    txt.setText("Вниз");
+                }else{
+                    txt.setText("Вверх");
+                }
             }
-        });
-
-        reduce.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                counter--;
-                TextView txt = (TextView) findViewById(R.id.txt);
-                txt.setText(Integer.toString(counter));
-            }
-        });
-
-        zero.setOnClickListener(new Button.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                counter = 0;
-                TextView txt = (TextView) findViewById(R.id.txt);
-                txt.setText(Integer.toString(counter));
-            }
-        });
+        }else{
+            Random random = new Random();
+            this.random = random.nextInt(9);
+            txt.setText("Вы проиграли. Вы можете начать игру! Выберите число");
+            this.tried = 0;
+        }
     }
 }
